8e3d8d9c313a3336bf46e4a8d149cd582517f2a9
Vikas Saurabh <vikas.saurabh@gmail.com>
2012-02-23 17:07:06 +0530

    using experimental feature in jetpack 1.5 to use trusted content in panel to avoid contentScript altogether

--- data/contentScript.js
+++
@@ -1,7 +0,0 @@
-self.port.on("myAddonScriptEvent", function(payloadFromAddon)
-{
-	if(payloadFromAddon["event"]=="showQR")
-	{
-		document.defaultView.postMessage(JSON.stringify(payloadFromAddon), "*");
-	}
-});
